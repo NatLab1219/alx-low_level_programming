a580c5bb2fcef9a889b2ce7254843376183e0f25
NatLab1219 <105284888+NatLab1219@users.noreply.github.com>
2022-12-15 04:51:16 -0800

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * largest_number -return the largest of 3 numbers
+ * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
  * Return: largest number
  */
 
-int largerst_number(int a, int b, int c)
+int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
@@ -24,5 +24,6 @@ int largerst_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
