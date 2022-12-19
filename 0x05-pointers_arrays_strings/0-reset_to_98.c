30bf7bc23ffa2b16825a4b6c07efd853fafe00c6
NatLab1219 <natisami1219com>
2022-12-19 06:34:54 -0800

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* main - reset to 98
-* @n: is input ineger
-* Return - nothing
-*/
+ * reset_to_98 - takes a pointer to a parameter and updates its value.
+ * @n: input integer.
+ * Return: no return.
+ */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
